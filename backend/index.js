bdd30861881b9e4b22b3acc1a2361cc263fef0c0
Vanessa <vahnkoch@gmail.com>
2020-03-23 23:39:58 -0300

    criando o projeto

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({
+    evento: 'Semana Omnistack 11.0',
+    aluno: 'Vanessa Ribeiro Koch'
+  });
+});
+
+app.listen(3333);
+
